10b48073b300f0567e7f5efeed81f28cdd0f950f
Giuseppe <degiorgigiuseppe@rocketmail.com>
2018-10-08 23:31:09 +0200

    Commit di prova fatto dal Mac di giuseppe

--- src/PrimaClasseProvaGit.java
+++ src/PrimaClasseProvaGit.java
@@ -3,7 +3,8 @@ public class PrimaClasseProvaGit {
 	public static void main(String [] args)
 	{
 		System.out.println("provaTestGit-Main");
-		System.out.println("provaTestGit");//ciao
+		System.out.println("Mac di Giuseppe");
+		
 
 	}
 }
